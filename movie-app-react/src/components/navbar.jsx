230bbc044981c758e9a5e08ca7827e3922b7cb5f
Anna Vass <panni.vass@gmail.com>
2024-06-02 19:43:28 +0200

    fixed mobile nav

--- movie-app-react/src/components/navbar.jsx
+++ movie-app-react/src/components/navbar.jsx
@@ -112,17 +112,14 @@ export const Navbar = () => {
                     <DisclosurePanel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
                             {navigation.map((item) => (
-                                <DisclosureButton
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block rounded-md px-3 py-2 text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                >
-                                    {item.name}
+                                <DisclosureButton>
+                                    <NavLink
+                                        key={item.href}
+                                        to={item.href}
+                                        className={({ isActive }) => classNames(
+                                            isActive ? 'bg-blue-gray-800 text-white' : 'text-gray-300 hover:bg-blue-gray-600 hover:text-white',
+                                            'rounded-md px-3 py-2 text-base font-medium')}
+                                    >{item.name}</NavLink>
                                 </DisclosureButton>
                             ))}
                         </div>
